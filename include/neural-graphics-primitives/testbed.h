7c05a778a4201f462b1f2463fe2f4e37d10d312b
yindaheng98 <yindaheng98@gmail.com>
2024-01-13 22:29:44 -0800

    density grid basic

--- include/neural-graphics-primitives/testbed.h
+++ include/neural-graphics-primitives/testbed.h
@@ -457,8 +457,10 @@ public:
 	pybind11::array_t<float> screenshot(bool linear, bool front_buffer) const;
 	void override_sdf_training_data(pybind11::array_t<float> points, pybind11::array_t<float> distances);
 	void load_params(pybind11::array_t<float> params, pybind11::array_t<int> index);
+	void load_density_grid(pybind11::array_t<float> density_grid, pybind11::array_t<int> index);
 #endif
 	void set_params(float* params, int* index, size_t n);
+	void set_density_grid(float* density_grid, int* index, size_t n);
 
 	double calculate_iou(uint32_t n_samples=128*1024*1024, float scale_existing_results_factor=0.0, bool blocking=true, bool force_use_octree = true);
 	void draw_visualizations(ImDrawList* list, const mat4x3& camera_matrix);
@@ -1216,6 +1218,7 @@ public:
 	std::shared_ptr<NerfNetwork<network_precision_t>> m_nerf_network;
 	GPUMemory<float> params_gpu; // yin: for ngp flow
 	GPUMemory<int> index_gpu; // yin: for ngp flow
+	GPUMemory<float> density_grid_gpu; // yin: for ngp flow
 };
 
 }
--- src/python_api.cu
+++ src/python_api.cu
@@ -53,6 +53,17 @@ void Testbed::load_params(py::array_t<float> params, py::array_t<int> index) { /
 	set_params(static_cast<float*>(params_buf.ptr), static_cast<int*>(index_buf.ptr), index_buf.shape[0]);
 }
 
+void Testbed::load_density_grid(pybind11::array_t<float> density_grid, pybind11::array_t<int> index) { // yin: for ngp flow
+	py::buffer_info density_grid_buf = density_grid.request();
+	py::buffer_info index_buf = index.request();
+
+	if (density_grid_buf.ndim != 1 || index_buf.ndim != 1 || density_grid_buf.shape[0] != index_buf.shape[0]) {
+		tlog::error() << "Invalid Density_grid<->Index data";
+		return;
+	}
+	set_density_grid(static_cast<float*>(density_grid_buf.ptr), static_cast<int*>(index_buf.ptr), index_buf.shape[0]);
+}
+
 void Testbed::Nerf::Training::set_image(int frame_idx, pybind11::array_t<float> img, pybind11::array_t<float> depth_img, float depth_scale) {
 	if (frame_idx < 0 || frame_idx >= dataset.n_images) {
 		throw std::runtime_error{"Invalid frame index"};
@@ -446,6 +457,7 @@ PYBIND11_MODULE(pyngp, m) {
 		)
 		.def("override_sdf_training_data", &Testbed::override_sdf_training_data, "Override the training data for learning a signed distance function")
 		.def("load_params", &Testbed::load_params, "Load params at any time.")
+		.def("load_density_grid", &Testbed::load_density_grid, "Load density grid at any time.")
 		.def("calculate_iou", &Testbed::calculate_iou, "Calculate the intersection over union error value",
 			py::arg("n_samples") = 128*1024*1024,
 			py::arg("scale_existing_results_factor") = 0.0f,
